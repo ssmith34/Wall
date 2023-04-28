7131054f0d5f0fe93f39d74aab8ec375a88c44c8
Shane Smith <110691416+ssmith34@users.noreply.github.com>
2023-04-28 13:08:19 -0400

    Finished project

--- Wall.java
+++ Wall.java
@@ -1,3 +1,38 @@
 public class Wall {
-    
+    private double width;
+    private double height;
+
+    public Wall(double width, double height) {
+        setHeight(height);
+        setWidth(width);
+    }
+
+    public Wall() {
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+
+        if (width < 0)
+            this.width = 0;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+
+        if (height < 0)
+            this.height = 0;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
 }
